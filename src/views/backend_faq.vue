ca257f829974c438d4edc364d5c5f1efff64349f
hsuanhl <shuanhui.l@gmail.com>
2023-04-07 14:12:05 +0800

    fix: add meow meow page when RWD

--- src/views/backend_faq.vue
+++ src/views/backend_faq.vue
@@ -283,11 +283,22 @@
         </div>
 
         <!-- 992以下顯示內容 -->
-        <div class="main rest-inf">
-            不支援手機喔
-
-        </div>
     </main>
+        <div :class="bodyClass" class="nosupport">
+            <main>
+                <section class="noSupport">
+                    <a href="#">
+                        <div>     
+                            <img class="LiIcon" src="../assets/img/LiIcon.png" alt="">
+                            <h1>歹勢！後台目前不支援手機</h1>
+                            <img class="cat" src="../assets/img/Cat.png" alt="">
+                            <p>下班請休息，我們明天再忙！</p>
+                        </div>
+                    </a>
+                </section>
+            </main>
+         
+      </div>
 </div>
     <Footer></Footer>
 </template>
