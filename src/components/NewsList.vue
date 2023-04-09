7754858023674c2a72c207ea9862e0f111d00961
R6NNN <eloisewang152@gmail.com>
2023-04-09 11:57:12 +0800

    fix sass loader in App.vue

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,15 +1,15 @@
 <template>
-    <a href="#">
+    <a href="#" >
       <article class="news_item">
         <section>
           <section class="article_news">
-            <div class="tag tag-orange">{{news.CATEGORY}} 公告</div>
-            <p class="rwd-newsdate">{{news.CREATE_TIME}} 2022-03-11</p>
-            <h2>{{news.TITLE}}清明期間 大湖里第一公墓管制出入</h2>
-            <h5>{{news.CREATE_TIME}}2022-01-01</h5>
+            <div class="tag tag-orange">{{news.CATEGORY}}</div>
+            <p class="rwd-newsdate">{{news.CREATE_TIME}}</p>
+            <h2>{{news.TITLE}}</h2>
+            <h5>{{news.CREATE_TIME}}</h5>
           </section>
           <div class="image list_pic">
-            <!-- <img :src="require(`../assets/img/${news.PIC}`)" alt=""> -->
+            <!-- <img :src="require(`@/assets/img/${news.PIC}`)" alt=""> -->
           </div>
         </section>
       </article>
@@ -22,12 +22,14 @@
 export default {
     data(){
         return {                 
-            news: [{
+            news: [
+            {
             "CATEGORY": "公告",
             "CREATE_TIME": "2023-03-02",
             "TITLE": "清明期間 大湖里第一公墓管制出入",
-            "PIC": "c_1_food.jpg"
-            }],
+            "PIC": "c_1_food.jpg",
+            },
+        ],
         }
             },
             methods: {
