4d7d17a2341fe9d9a70750c2fb1bd76aa2e73231
hsuanhl <shuanhui.l@gmail.com>
2023-04-22 01:37:31 +0800

    fix: backend login style

--- src/views/backend_login.vue
+++ src/views/backend_login.vue
@@ -1,45 +1,56 @@
 <template>
   <backendNavbar></backendNavbar>
-  <main>
-  
+  <div class="body-sec">
+    <main class="backpadding">
+      <!-- 後台登入 -->
+      <div class="center">
+        <h2>後台登入</h2>
+        <form>
+          <div class="txt_field">
+            <h5>帳號</h5>
+            <input
+              type="text"
+              class="f-text label-left"
+              id="name5"
+              placeholder="請輸入帳號"
+            />
 
-  <!-- 後台登入 -->
-<div class="center">
-    <h1>後台登入</h1>
-    <form>
-      <div class="txt_field">
-        <h5>帳號</h5>
-        <input type="text" class="f-text label-left" id="name5" placeholder="請輸入帳號">
-    
-        <label></label>
+            <label></label>
+          </div>
+          <div class="txt_field">
+            <h5>密碼</h5>
+            <input
+              type="password"
+              class="f-text label-left"
+              id="name5"
+              placeholder="請輸入密碼"
+            />
+            <label></label>
+            <!-- <label>
+              <div class="pass">
+                <a href="./sass_forgetpassword.html">忘記密碼</a>
+              </div>
+            </label> -->
+          </div>
+          <div class="login_btn">
+            <div class="logo_center">
+              <a href="./backend/index.html">
+                <button type="button" class="btn-m btn-color-green">
+                  登入
+                </button></a
+              >
+            </div>
+          </div>
+        </form>
       </div>
-      <div class="txt_field">
-        <h5>密碼</h5>
-        <input type="password" class="f-text label-left" id="name5" placeholder="請輸入密碼">
-        <label></label>
-        <label> <div class="pass"><a href="./sass_forgetpassword.html">忘記密碼</a> </div> </label>
-      </div>
-      <div class="login_btn">
-      <div class="logo_center">
-       
-     <a href="./backend/index.html">   <button type="button" class="btn-m btn-color-green">登入</button></a>
-    </div>
-
-      </div>
-    </form>
+    </main>
   </div>
-
-
-
-
-        </main>
-      
-  <Footer></Footer>  
+  <Footer></Footer>
 </template>
 
 <script>
 import backendNavbar from "./backendNavbar.vue";
-import Footer from './Footer.vue';
+import Footer from "./Footer.vue";
 
 export default {
   components: {
@@ -47,11 +58,11 @@ export default {
     Footer,
   },
   mounted() {
-    let openBtn = document.getElementById('openBtn');
-    let hiddenRow = document.querySelector('.hidden-row');
+    let openBtn = document.getElementById("openBtn");
+    let hiddenRow = document.querySelector(".hidden-row");
 
-    openBtn.addEventListener('click', () => {
-        hiddenRow.classList.toggle('show-row');
+    openBtn.addEventListener("click", () => {
+      hiddenRow.classList.toggle("show-row");
     });
   },
 };
