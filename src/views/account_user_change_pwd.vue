2bae2b123e14db71a89a1a2abc736633c1b431f1
Amelie Kuo <amelie.kkuo@gmail.com>
2023-04-22 22:30:09 +0800

    fix account page

--- src/views/account_user_change_pwd.vue
+++ src/views/account_user_change_pwd.vue
@@ -5,7 +5,7 @@
         <main>
         <section class="account-border">
             <div class="account">
-                <Accountsidebar :PORTRAIT="PORTRAIT"></Accountsidebar>
+                <Accountsidebar :localPORTRAIT="localPORTRAIT"></Accountsidebar>
                 <section class="account-content">
                     <h1 class="marginbottom30">變更密碼</h1>
                 
@@ -83,47 +83,50 @@ import navbar from './navbar.vue';
 import Accountsidebar from '@/components/Accountsidebar.vue';
 export default {
      data(){
-                   return {
-                    name:'',
-                    nickname:'',   
-                    gender:'',
-                    IDnumber:'',
-                    birthdate:'',
-                    email:'',
-                    phonenumber:'',
+        return {
+            name:'',
+            nickname:'',   
+            gender:'',
+            IDnumber:'',
+            birthdate:'',
+            email:'',
+            phonenumber:'',
+            localPORTRAIT:"",
 
-                    datasTrs:[
-                        'No.', '姓名', '帳號', '電子信箱', '編輯',  '刪除'
-                    ],
+            datasTrs:[
+                'No.', '姓名', '帳號', '電子信箱', '編輯',  '刪除'
+            ],
 
-                    datas:[
-                        {"0": 1, "1": "王一明", "2": "w1account123", "3": "q1w2e3r4@email.com"},
-                        {"0": 2, "1": "王二明", "2": "leeaccount1231111", "3": "zxcvb1245@mailbox.com"},
-                    ],
+            datas:[
+                {"0": 1, "1": "王一明", "2": "w1account123", "3": "q1w2e3r4@email.com"},
+                {"0": 2, "1": "王二明", "2": "leeaccount1231111", "3": "zxcvb1245@mailbox.com"},
+            ],
 
-                    content:'',
-                inputValFromChild: [{}],
-               
-                   }
+            content:'',
+            inputValFromChild: [{}],
 
-                   
-
-                   },
-                     methods: {
-                        clearCookies() {
-                        // 取得目前的 cookie 字串
-                        let cookies = document.cookie;
-                        // 將 cookie 字串分割成每個 cookie
-                        let cookieArr = cookies.split("; ");
-                        // 迭代 cookieArr，將每個 cookie 都設置過期時間為過去的日期，使其被刪除
-                        for (let i = 0; i < cookieArr.length; i++) {
-                        let cookie = cookieArr[i];
-                        let eqPos = cookie.indexOf("=");
-                        let name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-                        document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
-                        }
-                    },
-                    },
-                    components:{navbar,Accountsidebar} 
+        }
+    },
+    methods: {
+        clearCookies() {
+            // 取得目前的 cookie 字串
+            let cookies = document.cookie;
+            // 將 cookie 字串分割成每個 cookie
+            let cookieArr = cookies.split("; ");
+            // 迭代 cookieArr，將每個 cookie 都設置過期時間為過去的日期，使其被刪除
+            for (let i = 0; i < cookieArr.length; i++) {
+            let cookie = cookieArr[i];
+            let eqPos = cookie.indexOf("=");
+            let name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+            document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
+            }
+        },
+    },
+    components:{
+        navbar,Accountsidebar
+    },
+    mounted(){
+        this.localPORTRAIT = localStorage.getItem("portrait");
+    },
 }
 </script>
