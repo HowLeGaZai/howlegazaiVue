3e9ef53e90ecbd05eb9ab938d252dbccacd8843e
Amelie Kuo <amelie.kkuo@gmail.com>
2023-04-23 16:51:43 +0800

    add account_check_pwd

--- src/views/account_user_change_pwd.vue
+++ src/views/account_user_change_pwd.vue
@@ -8,67 +8,55 @@
                 <Accountsidebar :localPORTRAIT="localPORTRAIT"></Accountsidebar>
                 <section class="account-content">
                     <h1 class="marginbottom30">變更密碼</h1>
-                
-                  <div class="changePwd_Content">
-                    <div class="row account_row account_row2">
-                      <div class="col-md-3 col-12">
-                         
-                      </div>
-                      <div class="col-md-6 col-12">
-                          <h4>目前的密碼* (英文+數字6-8字)</h4>
-                          <input type="text" class="f-text nomargin" id="pwd" v-model="pwd" placeholder="請輸入目前的密碼" >
-                      </div>
-                      <div class="col-md-3 col-12">
-                          
-                      </div>
-                    </div>
-                    <div class="row account_row account_row2">
-                      <div class="col-md-3 col-12">
-                         
-                      </div>
-                      <div class="col-md-6 col-12">
-                          <h4>新密碼* (英文+數字6-8字)</h4>
-                          <input type="text" class="f-text nomargin" id="newpwd" v-model="newpwd" placeholder="請輸入密碼" >
-                      </div>
-                      <div class="col-md-3 col-12">
-                          
-                      </div>
-                    </div>
-                    <div class="row account_row account_row2">
-                      <div class="col-md-3 col-12">
-                         
-                      </div>
-                      <div class="col-md-6 col-12">
-                          <h4>目前的密碼* (英文+數字6-8字)</h4>
-                          <input type="text" class="f-text nomargin" id="newpwd2" v-model="newpwd2" placeholder="請再次輸入密碼" >
-                      </div>
-                      <div class="col-md-3 col-12">
-                          
-                      </div>
-                    </div>
+                    <div class="changePwd_Content">
+                        <div class="">
+                            <h4>舊密碼<span v-if="!password"><i class="bi bi-asterisk"></i></span><span v-if="passwordDuplicate" class="red">密碼錯誤</span></h4>
+                            <input
+                            type="password"
+                            class="f-text nomargin"
+                            id="sPassword"
+                            placeholder="請輸入未更改前密碼"
+                            required
+                            v-model="oldpassword"
+                            maxlength="12"
+                            @keyup="checkOldPassword"
+                            >
+                            <i class="bi bi-eye-slash-fill" @click="openpwd"></i>
+                            <!-- @keyup="checkOldPassword" -->
+                            <!-- <input type="text" class="f-text nomargin" id="pwd" v-model="pwd" placeholder="請輸入原先密碼" > -->
+                        </div>
+                        <div class="">
+                            <h4>新密碼<span v-if="!password"><i class="bi bi-asterisk"></i></span><span v-if="!passwordValid">*8~12字元，需包含英文小寫和數字</span></h4>
+                            <input
+                                    type="password"
+                                    class="f-text nomargin"
+                                    id="sPassword"
+                                    placeholder="8~12字元，需包含英文小寫和數字"
+                                    required
+                                    v-model.lazy.trim="newpassword"
+                                    maxlength="12"
+                                    @blur="validatePassword(newpassword)"
+                                ><i class="bi bi-eye-slash-fill" @click="openpwd"></i>
+                            <!-- <input type="text" class="f-text nomargin" id="newpwd" v-model="newpwd" placeholder="8~12字元，需包含英文小寫和數字" > -->
+                        </div>
+                        <div class="">
+                            <h4>再次輸入新密碼<span v-if="!password"><i class="bi bi-asterisk"></i></span><span v-if="!passwordValid">*8~12字元，需包含英文小寫和數字</span></h4>
+                            <input
+                                    type="password"
+                                    class="f-text nomargin"
+                                    id="sPassword"
+                                    placeholder="請再輸入一次"
+                                    required
+                                    v-model.lazy.trim="newpasswordDouble"
+                                    maxlength="12"
+                                    @blur="validatePassword(newpasswordDouble)"
+                                ><i class="bi bi-eye-slash-fill" @click="openpwd"></i>
+                                <!-- bi-eye-fill -->
+                            <!-- <input type="text" class="f-text nomargin" id="newpwd2" v-model="newpwd2" placeholder="請再輸入一次" > -->
+                        </div>
+                        <button class="btn-m btn-color-green marginbottom20">送出修改</button>
 
-                    <button class="btn-m btn-color-green marginbottom20">送出修改</button>
-                     
                   </div>  
-                     
-                      
-
-
-
-                      
-
-               
-
-                
-
-                      
-              
-                  
-
-
-
-
-
                 </section>
             </div>
         </section>
@@ -78,57 +66,120 @@
 </template>
 
 <script>
-
-// 測試測試
-
 import navbar from './navbar.vue';
 import Accountsidebar from '@/components/Accountsidebar.vue';
+
 export default {
      data(){
         return {
-            name:'',
-            nickname:'',   
-            gender:'',
-            IDnumber:'',
-            birthdate:'',
-            email:'',
-            phonenumber:'',
             localPORTRAIT:"",
+            oldpassword:'',
+            newpassword:'',
+            newpasswordDouble:'',
 
-            datasTrs:[
-                'No.', '姓名', '帳號', '電子信箱', '編輯',  '刪除'
-            ],
+            userId:'',
+            updater:'',
 
-            datas:[
-                {"0": 1, "1": "王一明", "2": "w1account123", "3": "q1w2e3r4@email.com"},
-                {"0": 2, "1": "王二明", "2": "leeaccount1231111", "3": "zxcvb1245@mailbox.com"},
-            ],
+            passwordValid: true,
+            passwordDuplicate:false,
 
-            content:'',
-            inputValFromChild: [{}],
+            datas:[],
 
         }
     },
     methods: {
-        clearCookies() {
-            // 取得目前的 cookie 字串
-            let cookies = document.cookie;
-            // 將 cookie 字串分割成每個 cookie
-            let cookieArr = cookies.split("; ");
-            // 迭代 cookieArr，將每個 cookie 都設置過期時間為過去的日期，使其被刪除
-            for (let i = 0; i < cookieArr.length; i++) {
-            let cookie = cookieArr[i];
-            let eqPos = cookie.indexOf("=");
-            let name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-            document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
+
+        getCookieValue(cookieName) {
+            const cookies = document.cookie.split("; ");
+            for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].split("=");
+            if (cookie[0] === cookieName) {
+                return cookie[1];
+            }
+            }
+            return null;
+        },
+
+        validatePassword(password) {
+            // 檢查單一密碼欄位是否符合指定格式
+            const passwordRegex = /^(?=.*[a-z])(?=.*\d)[a-z\d]{8,12}$/i; // 英文小寫+數字，8-12碼
+
+            if (!passwordRegex.test(password)) {
+                this.passwordValid = false;
+                // this.password = "";
+                return;
+            }
+            this.passwordValid = true;
+        },
+
+        
+        async checkOldPassword(){
+
+            try {
+                const userId = this.getCookieValue("id");
+                const oldpassword = this.oldpassword;
+
+                console.log(userId);
+                console.log(oldpassword)
+
+                const formData2 = new FormData()
+                formData2.append('OLDPASSWORD', this.oldpassword)
+                formData2.append('userId', this.userId)
+                const response = await
+                
+                axios.post('http://localhost/TGD104G1/public/API/account_check_pwd.php', formData2);
+                    const result = response.data;
+                    console.log(result);
+                    if (result === 'nosame') {
+                        // 密碼不一致
+                        this.passwordDuplicate =  true; // 設定為 true
+                    
+                    } else {
+                        // 密碼一致
+                        this.passwordDuplicate = false; // 設定為 false
+                    }
+                    } catch (error) {
+                        console.error(error);
+                    // 處理錯誤
+                    }
+        },
+
+        openpwd(event) {
+            // 取得 i 元素
+            const icon = event.target;
+            // 切換 i 元素的類別
+            icon.classList.toggle("bi-eye-fill");
+            icon.classList.toggle("bi-eye-slash-fill");
+
+            // 取得要修改的 input 元素
+            const passwordInput = icon.previousSibling;
+
+            if (passwordInput.type === "password") {
+                passwordInput.setAttribute("type", "text");
+            } else {
+                passwordInput.setAttribute("type", "password");
             }
         },
+        // clearCookies() {
+        //     // 取得目前的 cookie 字串
+        //     let cookies = document.cookie;
+        //     // 將 cookie 字串分割成每個 cookie
+        //     let cookieArr = cookies.split("; ");
+        //     // 迭代 cookieArr，將每個 cookie 都設置過期時間為過去的日期，使其被刪除
+        //     for (let i = 0; i < cookieArr.length; i++) {
+        //     let cookie = cookieArr[i];
+        //     let eqPos = cookie.indexOf("=");
+        //     let name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        //     document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
+        //     }
+        // },
     },
     components:{
         navbar,Accountsidebar
     },
     mounted(){
         this.localPORTRAIT = localStorage.getItem("portrait");
+
     },
 }
 </script>
