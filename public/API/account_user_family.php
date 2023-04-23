0486956e7b9b162df95873d7360f127a8dbfb6b2
tommypanrap <liweidesigner@gmail.com>
2023-04-23 22:35:49 +0800

    新增成員

--- public/API/account_user_family.php
+++ public/API/account_user_family.php
@@ -8,11 +8,12 @@
     // $sql = "SELECT * FROM SPACE";
     $sql = "SELECT FULL_NAME, ACCOUNT ,EMAIL FROM USER 
             WHERE FAMILY_ID = (SELECT FAMILY_ID FROM USER WHERE ID = ?) 
-            AND USER_LEVEL = 2";
+            AND ID != ?";
 
     //執行並查詢，會回傳查詢結果的物件，必須使用fetch、fetchAll...等方式取得資料
     $statement = $pdo->prepare($sql);
     $statement->bindValue(1 , $ID); 
+    $statement->bindValue(2 , $ID); 
     $statement->execute();
 
     //抓出全部且依照順序封裝成一個二維陣列
