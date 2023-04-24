27ec02f279d68a716771209e7ce00f9e1d119106
PrroA <ga2006049738@gmail.com>
2023-04-24 15:28:04 +0800

    0424 首頁 里辦 渲染 RWD

--- public/API/index_news.php
+++ public/API/index_news.php
@@ -4,7 +4,8 @@ include("conn.php");
 
 //---------------------------------------------------
 
- $sql = "SELECT  CATEGORY, CREATE_TIME, TITLE, TOP FROM NEWS ORDER BY TOP DESC, CREATE_TIME DESC LIMIT 5;";
+$sql = "SELECT CATEGORY, CREATE_TIME, TITLE, TOP, ROUTER_ID FROM NEWS ORDER BY TOP DESC, CREATE_TIME DESC LIMIT 5;";
+
 
  //執行並查詢，會回傳查詢結果的物件，必須使用fetch、fetchAll...等方式取得資料
  $statement = $pdo->query($sql);
