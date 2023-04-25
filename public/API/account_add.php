fdde6f02e67940f5d6474db7037993abe77bb5f1
hsuanhl <shuanhui.l@gmail.com>
2023-04-25 20:42:19 +0800

    fix:account add for member

--- public/API/account_add.php
+++ public/API/account_add.php
@@ -3,7 +3,7 @@
 include("conn.php");
 
 //建立SQL語法
-$sql = "INSERT into USER VALUES(DEFAULT,?,?,?,'http://localhost/TGD104G1/src/assets/img/user_pic.png',NULL,?,?,NULL,?,?,NULL,2,DEFAULT,NULL,NULL,1,NOW(),NULL, ?,NULL,DEFAULT,DEFAULT,?)";
+$sql = "INSERT into USER VALUES(DEFAULT,?,?,?,'http://localhost/TGD104G1/src/assets/img/user_pic.png',NULL,?,?,NULL,?,?,?,2,DEFAULT,NULL,NULL,1,NOW(),NULL, ?,NULL,DEFAULT,DEFAULT,?)";
 
 $FIRST_NAME = $_POST['FIRST_NAME'];
 $LAST_NAME = $_POST['LAST_NAME'];
@@ -13,6 +13,7 @@ $PASSWORD = $_POST['PASSWORD'];
 $EMAIL = $_POST['EMAIL'];
 $ADDRESS = $_POST['ADDRESS'];
 $FAMILY_ID = $_POST['FAMILY_ID'];
+$NICKNAME = $FULL_NAME;
 
 
 
@@ -27,8 +28,11 @@ $statement->bindValue(4, $ACCOUNT);
 $statement->bindValue(5, $PASSWORD);
 $statement->bindValue(6, $EMAIL);
 $statement->bindValue(7, $ADDRESS);
-$statement->bindValue(8, $FAMILY_ID);
+$statement->bindValue(8, $NICKNAME);
 $statement->bindValue(9, $FAMILY_ID);
+$statement->bindValue(10, $FAMILY_ID);
+
+
 
 
 
