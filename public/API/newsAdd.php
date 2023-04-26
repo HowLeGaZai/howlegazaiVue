f32f2bc47750917690d031ae13f3e5250854b689
R6NNN <eloisewang152@gmail.com>
2023-04-26 16:00:46 +0800

    fix frontend account user delete function

--- public/API/newsAdd.php
+++ public/API/newsAdd.php
@@ -1,20 +1,21 @@
 <?php
 
 include("conn.php");
+//如果傳進來的$routerid傳進sql比對後是空值，就會執行update的語法
 
-$sql = "INSERT into NEWS VALUES(DEFAULT,?,?,?,?,?,?,NOW(),1,0)";
+
+// $sql = "INSERT into NEWS VALUES(DEFAULT,?,?,?,?,?,?,NOW(),1,0)";
+
 
 $routerid = $_POST['routerid'];
 $category= $_POST['category'];
 $title= $_POST['title'];
 $adminid= $_POST['adminid'];
-
 $content= $_POST['content'];
 $pic= $_POST['pic'];
 
 
 $statement = $pdo -> prepare($sql);
-
 $statement->bindValue(1, $routerid);
 $statement->bindValue(2, $category);
 $statement->bindValue(3, $title);
