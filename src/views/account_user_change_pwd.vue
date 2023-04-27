e7cf6606a76e313216c27c28ac838fa8d22c561d
hsuanhl <shuanhui.l@gmail.com>
2023-04-27 14:44:46 +0800

    fix: login portrait

--- src/views/account_user_change_pwd.vue
+++ src/views/account_user_change_pwd.vue
@@ -262,7 +262,7 @@ export default {
             let cookie = cookieArr[i];
             let eqPos = cookie.indexOf("=");
             let name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-            document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
+            document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT`;
             }
         },
     },
