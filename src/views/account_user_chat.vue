c7887f97b0c56839116874020fda822e03ec59e6
tommypanrap <liweidesigner@gmail.com>
2023-04-29 16:07:13 +0800

    改成v-show

--- src/views/account_user_chat.vue
+++ src/views/account_user_chat.vue
@@ -20,7 +20,7 @@
                                 {{data.TITLE}}
                                 </router-link></td>
                             <td data-label="">
-                                <router-link :to="{ name: 'chat_update', params: { Id: data.ID } }" v-if="data.STATUS == 1">
+                                <router-link :to="{ name: 'chat_update', params: { Id: data.ID } }" v-show="data.STATUS == 1">
                                     <button type="button" class="btn-icon"><i class="bi bi-pencil-square btn-font-color-green"></i></button>
                                 </router-link>
                                 <button @click="articleDel" type="button" class="btn-icon" v-if="data.STATUS != -1"><i class="bi bi-x-circle-fill btn-font-color-green"></i></button>
