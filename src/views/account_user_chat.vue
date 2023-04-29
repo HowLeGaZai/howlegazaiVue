d4ec1f14283ebaa48bad0caf7d75e2008588e48b
tommypanrap <liweidesigner@gmail.com>
2023-04-29 15:46:45 +0800

    fix account_user

--- src/views/account_user_chat.vue
+++ src/views/account_user_chat.vue
@@ -22,8 +22,6 @@
                             <td data-label="">
                                 <router-link :to="{ name: 'chat_update', params: { Id: data.ID } }" v-if="data.STATUS == 1">
                                     <button type="button" class="btn-icon"><i class="bi bi-pencil-square btn-font-color-green"></i></button>
-                                    
-                                    <!-- <button type="button" class="btn-icon"><i class="bi bi-pencil-square btn-font-color-green"></i></button> -->
                                 </router-link>
                                 <button @click="articleDel" type="button" class="btn-icon" v-if="data.STATUS != -1"><i class="bi bi-x-circle-fill btn-font-color-green"></i></button>
                             </td>
