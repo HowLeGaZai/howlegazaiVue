56848af9f8dd79c9fd2f471b47f423460bfe0923
tommypanrap <liweidesigner@gmail.com>
2023-05-01 12:30:57 +0800

    update signup

--- public/API/login.php
+++ public/API/login.php
@@ -1,42 +1,42 @@
 <?php
-    include("conn.php");
-    
-    $account = $_POST['account'];
-    $pwd = $_POST['password'];
-
-    // 建立 SQL
-    $sql = "SELECT * FROM USER WHERE STATUS = 1 and ACCOUNT = ? and PASSWORD = ?";
-
-    // 給值
-    $statement = $pdo->prepare($sql);
-    $statement->bindValue(1, $account);
-    $statement->bindValue(2, $pwd);
-    $statement->execute();
-    $data = $statement->fetchAll();
-
-    if(COUNT($data) > 0){
-        echo json_encode($data);
-    }else{
-        echo "N";
-    }
-
-
     // include("conn.php");
+    
     // $account = $_POST['account'];
     // $pwd = $_POST['password'];
 
-    // $sql = "SELECT * FROM USER WHERE STATUS = 1 and ACCOUNT = ? ";
+    // // 建立 SQL
+    // $sql = "SELECT * FROM USER WHERE STATUS = 1 and ACCOUNT = ? and PASSWORD = ?";
 
+    // // 給值
     // $statement = $pdo->prepare($sql);
     // $statement->bindValue(1, $account);
+    // $statement->bindValue(2, $pwd);
     // $statement->execute();
     // $data = $statement->fetchAll();
 
-    // if(COUNT($data) > 0 && password_verify($pwd, $data[0]['PASSWORD'])){
+    // if(COUNT($data) > 0){
     //     echo json_encode($data);
     // }else{
     //     echo "N";
     // }
+
+
+    include("conn.php");
+    $account = $_POST['account'];
+    $pwd = $_POST['password'];
+
+    $sql = "SELECT * FROM USER WHERE STATUS = 1 and ACCOUNT = ? ";
+
+    $statement = $pdo->prepare($sql);
+    $statement->bindValue(1, $account);
+    $statement->execute();
+    $data = $statement->fetchAll();
+
+    if(COUNT($data) > 0 && password_verify($pwd, $data[0]['PASSWORD'])){
+        echo json_encode($data);
+    }else{
+        echo "N";
+    }
 
 
 ?>
