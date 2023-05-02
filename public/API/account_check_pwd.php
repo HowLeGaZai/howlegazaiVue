c298635450689202f752b8c757efb2c2816a79b1
tommypanrap <liweidesigner@gmail.com>
2023-05-02 15:38:15 +0800

    commit

--- public/API/account_check_pwd.php
+++ public/API/account_check_pwd.php
@@ -21,16 +21,16 @@ $sql = "SELECT ID, PASSWORD
 //執行並查詢，會回傳查詢結果的物件，必須使用fetch、fetchAll...等方式取得資料
 
 $statement = $pdo->prepare($sql);
-// $statement->bindValue(1 , $ID);
-// $statement->bindValue(2 , $OLDPASSWORD);
+$statement->bindValue(1 , $ID);
+$statement->bindValue(2 , $OLDPASSWORD);
 $statement->execute();
 
 //抓出全部且依照順序封裝成一個二維陣列
 $data = $statement->fetchAll();
 
-// if ($data && password_verify($OLDPASSWORD, $data['PASSWORD'])) {
+if ($data && password_verify($OLDPASSWORD, $data['PASSWORD'])) {
 
-if ($data[0]['PASSWORD'] !== $OLDPASSWORD) {
+// if ($data[0]['PASSWORD'] !== $OLDPASSWORD) {
     // 密碼不一致
     echo "notsame";
 } else {
--- public/API/updatePassword.php
+++ public/API/updatePassword.php
@@ -9,6 +9,8 @@
     $newpwd = $_POST["newpwd"];
     $userid = $_POST["userid"];
 
+    $newpwd = password_hash($newpwd, PASSWORD_BCRYPT);
+
     // 抓出全部且依照順序封裝成一個二維陣列
     $statement = $pdo -> prepare($sql);
     $statement->bindValue(1, $newpwd);
